11abd1cd36d7c0ecc049107633d01bc7b58d2395
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-06-10 16:07:43 -0400

    added password validation color change

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 const hamburger = document.querySelector(".login");
 const form = document.querySelector("form.signup");
-
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm_password");
+const errorMessage = document.querySelector(".error-message");
 function myFunction() {
   if (hamburger.style.display === "flex") {
     hamburger.style.display = "none";
@@ -10,15 +12,26 @@ function myFunction() {
   }
 }
 form.addEventListener("submit", (e) => {
-  const password = document.querySelector(".password");
-  const confirmPassword = document.querySelector(".confirmPassword");
-  const errorMessage = document.querySelector(".error-message");
-  console.log("password recognized");
-  if (password !== confirmPassword) {
-    console.log("Passwords did not match. Please try again.");
+  if (password.value != confirmPassword.value) {
+    console.log(password.value + " and " + confirmPassword.value);
     e.preventDefault();
-    password.classList.add("error");
-    confirmPassword.classList.add("error");
+    // password.classList.add("error");
+    // confirmPassword.classList.add("error");
+    password.previousElementSibling.classList.add("error");
+    confirmPassword.previousElementSibling.classList.add("error");
+
+    password.previousElementSibling.classList.replace("valid", "error");
+    confirmPassword.previousElementSibling.classList.replace("valid", "error");
+
     errorMessage.style.display = "block";
+  } else {
+    e.preventDefault();
+
+    password.previousElementSibling.classList.add("valid");
+    confirmPassword.previousElementSibling.classList.add("valid");
+
+    password.previousElementSibling.classList.replace("error", "valid");
+    confirmPassword.previousElementSibling.classList.replace("error", "valid");
+    errorMessage.style.display = "none";
   }
 });
