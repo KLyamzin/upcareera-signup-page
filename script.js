7ebc30234aab8cfb95e2bb5fc7138765e52dc93a
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-06-10 13:21:42 -0400

    added password validation

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-let x = document.querySelector(".login");
+const hamburger = document.querySelector(".login");
+const form = document.querySelector("form.signup");
+
 function myFunction() {
-  if (x.style.display === "flex") {
-    x.style.display = "none";
+  if (hamburger.style.display === "flex") {
+    hamburger.style.display = "none";
   } else {
-    x.style.display = "flex";
+    hamburger.style.display = "flex";
   }
 }
+form.addEventListener("submit", (e) => {
+  const password = document.querySelector(".password");
+  const confirmPassword = document.querySelector(".confirmPassword");
+  const errorMessage = document.querySelector(".error-message");
+  console.log("password recognized");
+  if (password !== confirmPassword) {
+    console.log("Passwords did not match. Please try again.");
+    e.preventDefault();
+    password.classList.add("error");
+    confirmPassword.classList.add("error");
+    errorMessage.style.display = "block";
+  }
+});
